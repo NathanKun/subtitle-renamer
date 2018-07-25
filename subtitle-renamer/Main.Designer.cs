9206c94dffc650a75011f239e19154f39287ec67
HE Junyang <nathanhejunyang@gmail.com>
2018-07-25 16:16:44 +0200

    Add check btn and check result listview

--- subtitle-renamer/Main.Designer.cs
+++ subtitle-renamer/Main.Designer.cs
@@ -51,6 +51,9 @@
             this.labelSub = new System.Windows.Forms.Label();
             this.listViewMedia = new System.Windows.Forms.ListView();
             this.listViewSub = new System.Windows.Forms.ListView();
+            this.listViewCheck = new System.Windows.Forms.ListView();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.buttonRename = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelDirpath
@@ -82,7 +85,7 @@
             // checkBoxPrefix
             // 
             this.checkBoxPrefix.AutoSize = true;
-            this.checkBoxPrefix.Location = new System.Drawing.Point(68, 66);
+            this.checkBoxPrefix.Location = new System.Drawing.Point(68, 44);
             this.checkBoxPrefix.Name = "checkBoxPrefix";
             this.checkBoxPrefix.Size = new System.Drawing.Size(65, 21);
             this.checkBoxPrefix.TabIndex = 3;
@@ -93,7 +96,7 @@
             // checkBoxEpisode
             // 
             this.checkBoxEpisode.AutoSize = true;
-            this.checkBoxEpisode.Location = new System.Drawing.Point(254, 66);
+            this.checkBoxEpisode.Location = new System.Drawing.Point(254, 44);
             this.checkBoxEpisode.Name = "checkBoxEpisode";
             this.checkBoxEpisode.Size = new System.Drawing.Size(81, 21);
             this.checkBoxEpisode.TabIndex = 4;
@@ -104,7 +107,7 @@
             // checkBoxDelete
             // 
             this.checkBoxDelete.AutoSize = true;
-            this.checkBoxDelete.Location = new System.Drawing.Point(384, 66);
+            this.checkBoxDelete.Location = new System.Drawing.Point(384, 44);
             this.checkBoxDelete.Name = "checkBoxDelete";
             this.checkBoxDelete.Size = new System.Drawing.Size(71, 21);
             this.checkBoxDelete.TabIndex = 5;
@@ -115,7 +118,7 @@
             // checkBoxAdd
             // 
             this.checkBoxAdd.AutoSize = true;
-            this.checkBoxAdd.Location = new System.Drawing.Point(570, 66);
+            this.checkBoxAdd.Location = new System.Drawing.Point(570, 44);
             this.checkBoxAdd.Name = "checkBoxAdd";
             this.checkBoxAdd.Size = new System.Drawing.Size(55, 21);
             this.checkBoxAdd.TabIndex = 6;
@@ -129,7 +132,7 @@
             this.checkBoxExtension.Checked = true;
             this.checkBoxExtension.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBoxExtension.Enabled = false;
-            this.checkBoxExtension.Location = new System.Drawing.Point(942, 66);
+            this.checkBoxExtension.Location = new System.Drawing.Point(942, 44);
             this.checkBoxExtension.Name = "checkBoxExtension";
             this.checkBoxExtension.Size = new System.Drawing.Size(91, 21);
             this.checkBoxExtension.TabIndex = 7;
@@ -138,7 +141,7 @@
             // 
             // textBoxMediaExtension
             // 
-            this.textBoxMediaExtension.Location = new System.Drawing.Point(942, 93);
+            this.textBoxMediaExtension.Location = new System.Drawing.Point(942, 71);
             this.textBoxMediaExtension.Name = "textBoxMediaExtension";
             this.textBoxMediaExtension.Size = new System.Drawing.Size(80, 22);
             this.textBoxMediaExtension.TabIndex = 8;
@@ -146,7 +149,7 @@
             // 
             // textBoxSubExtension
             // 
-            this.textBoxSubExtension.Location = new System.Drawing.Point(942, 132);
+            this.textBoxSubExtension.Location = new System.Drawing.Point(942, 110);
             this.textBoxSubExtension.Name = "textBoxSubExtension";
             this.textBoxSubExtension.Size = new System.Drawing.Size(80, 22);
             this.textBoxSubExtension.TabIndex = 9;
@@ -154,21 +157,21 @@
             // 
             // textBoxSubAdd
             // 
-            this.textBoxSubAdd.Location = new System.Drawing.Point(570, 132);
+            this.textBoxSubAdd.Location = new System.Drawing.Point(570, 110);
             this.textBoxSubAdd.Name = "textBoxSubAdd";
             this.textBoxSubAdd.Size = new System.Drawing.Size(180, 22);
             this.textBoxSubAdd.TabIndex = 10;
             // 
             // textBoxSubDelete
             // 
-            this.textBoxSubDelete.Location = new System.Drawing.Point(384, 132);
+            this.textBoxSubDelete.Location = new System.Drawing.Point(384, 110);
             this.textBoxSubDelete.Name = "textBoxSubDelete";
             this.textBoxSubDelete.Size = new System.Drawing.Size(180, 22);
             this.textBoxSubDelete.TabIndex = 11;
             // 
             // textBoxSubEpStart
             // 
-            this.textBoxSubEpStart.Location = new System.Drawing.Point(254, 132);
+            this.textBoxSubEpStart.Location = new System.Drawing.Point(254, 110);
             this.textBoxSubEpStart.Name = "textBoxSubEpStart";
             this.textBoxSubEpStart.Size = new System.Drawing.Size(59, 22);
             this.textBoxSubEpStart.TabIndex = 12;
@@ -176,7 +179,7 @@
             // 
             // textBoxSubEpEnd
             // 
-            this.textBoxSubEpEnd.Location = new System.Drawing.Point(319, 132);
+            this.textBoxSubEpEnd.Location = new System.Drawing.Point(319, 110);
             this.textBoxSubEpEnd.Name = "textBoxSubEpEnd";
             this.textBoxSubEpEnd.Size = new System.Drawing.Size(59, 22);
             this.textBoxSubEpEnd.TabIndex = 13;
@@ -184,22 +187,24 @@
             // 
             // textBoxSubPrefix
             // 
-            this.textBoxSubPrefix.Location = new System.Drawing.Point(68, 132);
+            this.textBoxSubPrefix.Location = new System.Drawing.Point(68, 110);
             this.textBoxSubPrefix.Name = "textBoxSubPrefix";
             this.textBoxSubPrefix.Size = new System.Drawing.Size(180, 22);
             this.textBoxSubPrefix.TabIndex = 14;
+            this.textBoxSubPrefix.TextChanged += new System.EventHandler(this.textBoxSubPrefix_TextChanged);
             // 
             // textBoxSubSuffix
             // 
-            this.textBoxSubSuffix.Location = new System.Drawing.Point(756, 132);
+            this.textBoxSubSuffix.Location = new System.Drawing.Point(756, 110);
             this.textBoxSubSuffix.Name = "textBoxSubSuffix";
             this.textBoxSubSuffix.Size = new System.Drawing.Size(180, 22);
             this.textBoxSubSuffix.TabIndex = 16;
+            this.textBoxSubSuffix.TextChanged += new System.EventHandler(this.textBoxSubSuffix_TextChanged);
             // 
             // checkBoxSuffix
             // 
             this.checkBoxSuffix.AutoSize = true;
-            this.checkBoxSuffix.Location = new System.Drawing.Point(758, 66);
+            this.checkBoxSuffix.Location = new System.Drawing.Point(758, 44);
             this.checkBoxSuffix.Name = "checkBoxSuffix";
             this.checkBoxSuffix.Size = new System.Drawing.Size(64, 21);
             this.checkBoxSuffix.TabIndex = 15;
@@ -209,7 +214,7 @@
             // 
             // textBoxMediaPrefix
             // 
-            this.textBoxMediaPrefix.Location = new System.Drawing.Point(68, 93);
+            this.textBoxMediaPrefix.Location = new System.Drawing.Point(68, 71);
             this.textBoxMediaPrefix.Name = "textBoxMediaPrefix";
             this.textBoxMediaPrefix.Size = new System.Drawing.Size(180, 22);
             this.textBoxMediaPrefix.TabIndex = 17;
@@ -217,7 +222,7 @@
             // 
             // textBoxMediaSuffix
             // 
-            this.textBoxMediaSuffix.Location = new System.Drawing.Point(756, 93);
+            this.textBoxMediaSuffix.Location = new System.Drawing.Point(756, 71);
             this.textBoxMediaSuffix.Name = "textBoxMediaSuffix";
             this.textBoxMediaSuffix.Size = new System.Drawing.Size(180, 22);
             this.textBoxMediaSuffix.TabIndex = 18;
@@ -226,7 +231,7 @@
             // labelMedia
             // 
             this.labelMedia.AutoSize = true;
-            this.labelMedia.Location = new System.Drawing.Point(13, 96);
+            this.labelMedia.Location = new System.Drawing.Point(13, 74);
             this.labelMedia.Name = "labelMedia";
             this.labelMedia.Size = new System.Drawing.Size(46, 17);
             this.labelMedia.TabIndex = 19;
@@ -235,7 +240,7 @@
             // labelSub
             // 
             this.labelSub.AutoSize = true;
-            this.labelSub.Location = new System.Drawing.Point(13, 132);
+            this.labelSub.Location = new System.Drawing.Point(13, 110);
             this.labelSub.Name = "labelSub";
             this.labelSub.Size = new System.Drawing.Size(33, 17);
             this.labelSub.TabIndex = 20;
@@ -243,7 +248,7 @@
             // 
             // listViewMedia
             // 
-            this.listViewMedia.Location = new System.Drawing.Point(16, 171);
+            this.listViewMedia.Location = new System.Drawing.Point(16, 149);
             this.listViewMedia.Name = "listViewMedia";
             this.listViewMedia.Size = new System.Drawing.Size(492, 267);
             this.listViewMedia.TabIndex = 21;
@@ -252,18 +257,49 @@
             // 
             // listViewSub
             // 
-            this.listViewSub.Location = new System.Drawing.Point(530, 171);
+            this.listViewSub.Location = new System.Drawing.Point(530, 149);
             this.listViewSub.Name = "listViewSub";
             this.listViewSub.Size = new System.Drawing.Size(492, 267);
             this.listViewSub.TabIndex = 22;
             this.listViewSub.UseCompatibleStateImageBehavior = false;
             this.listViewSub.View = System.Windows.Forms.View.List;
             // 
+            // listViewCheck
+            // 
+            this.listViewCheck.Location = new System.Drawing.Point(16, 429);
+            this.listViewCheck.Name = "listViewCheck";
+            this.listViewCheck.Size = new System.Drawing.Size(1006, 267);
+            this.listViewCheck.TabIndex = 23;
+            this.listViewCheck.UseCompatibleStateImageBehavior = false;
+            this.listViewCheck.View = System.Windows.Forms.View.List;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(13, 706);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(75, 23);
+            this.buttonCheck.TabIndex = 24;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Location = new System.Drawing.Point(106, 706);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(75, 23);
+            this.buttonRename.TabIndex = 25;
+            this.buttonRename.Text = "Rename!";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1037, 450);
+            this.ClientSize = new System.Drawing.Size(1037, 741);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.listViewCheck);
             this.Controls.Add(this.listViewSub);
             this.Controls.Add(this.listViewMedia);
             this.Controls.Add(this.labelSub);
@@ -319,6 +355,9 @@
         private System.Windows.Forms.Label labelSub;
         private System.Windows.Forms.ListView listViewMedia;
         private System.Windows.Forms.ListView listViewSub;
+        private System.Windows.Forms.ListView listViewCheck;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Button buttonRename;
     }
 }
 
--- subtitle-renamer/Main.cs
+++ subtitle-renamer/Main.cs
@@ -17,8 +17,8 @@ namespace subtitle_renamer
         private List<string> mediaFiles = new List<string>();
         private List<string> subFiles = new List<string>();
 
-        private string partToDelete = "";
         private string partToAdd = "";
+        private string partToDelete = "";
 
         public Main()
         {
@@ -89,6 +89,10 @@ namespace subtitle_renamer
             return list;
         }
 
+        /**
+         * Files Filters
+         */
+
         private void FilterMediaFiles()
         {
             string ext = textBoxMediaExtension.Text.Trim();
@@ -130,16 +134,89 @@ namespace subtitle_renamer
                 }
                 UpdateListView(mediaFiles, listViewMedia);
 
-                // partToDelete
+                // calculate partToAdd
                 if (mediaFiles.Count > 0 && canStartsWith.Count > 0)
                 {
                     var start = canStartsWith[0].Length;
                     var len = mediaFiles[0].Length - shouldEndsWith.Length - start;
-                    partToDelete = mediaFiles[0].Substring(start, len);
+                    partToAdd = mediaFiles[0].Substring(start, len);
+
+                    if (partToAdd.Length > 0)
+                    {
+                        partToAdd += " etc";
+                    }
+
+                    textBoxSubAdd.Text = partToAdd;
+                }
+            }
+        }
+
+        private void FilterSubFiles()
+        {
+            string ext = textBoxSubExtension.Text.Trim();
+            string prefix = checkBoxPrefix.Checked ? textBoxSubPrefix.Text : "";
+            string suffix = checkBoxSuffix.Checked ? textBoxSubSuffix.Text : "";
+            List<string> eps = checkBoxEpisode.Checked ? GenerateEpisodeArray(textBoxSubEpStart.Text, textBoxSubEpEnd.Text) : null;
+
+            var canStartsWith = new List<string>();
+            if (eps == null)
+            {
+                canStartsWith.Add(prefix);
+            }
+            else
+            {
+                foreach (string ep in eps)
+                {
+                    canStartsWith.Add(prefix + ep);
+                }
+            }
+
+            var shouldEndsWith = suffix + ext;
+
+            if (ext.Length != 0)
+            {
+                subFiles.Clear();
+                foreach (string f in files)
+                {
+                    if (f.EndsWith(shouldEndsWith))
+                    {
+                        foreach (string start in canStartsWith)
+                        {
+                            if (f.StartsWith(start))
+                            {
+                                subFiles.Add(f);
+                                break;
+                            }
+                        }
+                    }
+                }
+                UpdateListView(subFiles, listViewSub);
+
+                // calculate partToAdd
+                if (subFiles.Count > 0 && canStartsWith.Count > 0)
+                {
+                    var start = canStartsWith[0].Length;
+                    var len = subFiles[0].Length - shouldEndsWith.Length - start;
+                    partToDelete = subFiles[0].Substring(start, len);
+
+                    if (partToDelete.Length > 0)
+                    {
+                        partToDelete += " etc";
+                    }
+
                     textBoxSubDelete.Text = partToDelete;
                 }
             }
         }
+
+
+        /**
+         * LISTENERS
+         */
+
+        /**
+         * Choose file button onclick
+         */
 
         private void buttonBrowse_Click(object sender, EventArgs e)
         {
@@ -168,10 +245,13 @@ namespace subtitle_renamer
                     checkBoxEpisode.Enabled = true;
                     checkBoxSuffix.Enabled = true;
 
-                    checkBoxDelete.Enabled = true;
+                    //checkBoxAdd.Enabled = true;
+                    //checkBoxDelete.Enabled = true;
                 }
             }
         }
+
+
 
         /**
          * Extension textboxes changed
@@ -204,8 +284,55 @@ namespace subtitle_renamer
                 return;
             }
 
-            FilterMediaFiles();
+            FilterSubFiles();
         }
+
+
+
+        /**
+         * Checkboxes changed
+         */
+
+        private void checkBoxPrefix_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxMediaPrefix.Enabled = checkBoxPrefix.Checked;
+            textBoxSubPrefix.Enabled = checkBoxPrefix.Checked;
+
+            FilterMediaFiles();
+            FilterSubFiles();
+        }
+
+        private void checkBoxEpisode_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxSubEpStart.Enabled = checkBoxEpisode.Checked;
+            textBoxSubEpEnd.Enabled = checkBoxEpisode.Checked;
+
+            FilterMediaFiles();
+            FilterSubFiles();
+        }
+
+        private void checkBoxDelete_CheckedChanged(object sender, EventArgs e)
+        {
+            FilterMediaFiles();
+            FilterSubFiles();
+        }
+
+        private void checkBoxAdd_CheckedChanged(object sender, EventArgs e)
+        {
+            FilterMediaFiles();
+            FilterSubFiles();
+        }
+
+        private void checkBoxSuffix_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxMediaSuffix.Enabled = checkBoxSuffix.Checked;
+            textBoxSubSuffix.Enabled = checkBoxSuffix.Checked;
+
+            FilterMediaFiles();
+            FilterSubFiles();
+        }
+
+
 
         /**
          * Media inputs changed
@@ -221,55 +348,38 @@ namespace subtitle_renamer
             FilterMediaFiles();
         }
 
+
+
+        /**
+         * Sub inputs changed
+         */
+
         private void textBoxSubEpStart_TextChanged(object sender, EventArgs e)
         {
             FilterMediaFiles();
+            FilterSubFiles();
         }
 
         private void textBoxSubEpEnd_TextChanged(object sender, EventArgs e)
         {
             FilterMediaFiles();
+            FilterSubFiles();
         }
 
-
-        /**
-         * Checkboxes changed
-         */
-
-        private void checkBoxPrefix_CheckedChanged(object sender, EventArgs e)
+        private void textBoxSubPrefix_TextChanged(object sender, EventArgs e)
         {
-            textBoxMediaPrefix.Enabled = checkBoxPrefix.Checked;
-            textBoxSubPrefix.Enabled = checkBoxPrefix.Checked;
-
-            FilterMediaFiles();
+            FilterSubFiles();
         }
 
-        private void checkBoxEpisode_CheckedChanged(object sender, EventArgs e)
+        private void textBoxSubSuffix_TextChanged(object sender, EventArgs e)
         {
-            textBoxSubEpStart.Enabled = checkBoxEpisode.Checked;
-            textBoxSubEpEnd.Enabled = checkBoxEpisode.Checked;
-
-            FilterMediaFiles();
+            FilterSubFiles();
         }
 
-        private void checkBoxDelete_CheckedChanged(object sender, EventArgs e)
+        private void buttonCheck_Click(object sender, EventArgs e)
         {
-            FilterMediaFiles();
-        }
+            var results = new List<string>();
 
-        private void checkBoxAdd_CheckedChanged(object sender, EventArgs e)
-        {
-            //textBoxSubAdd.Enabled = checkBoxAdd.Checked;
-
-            FilterMediaFiles();
-        }
-
-        private void checkBoxSuffix_CheckedChanged(object sender, EventArgs e)
-        {
-            textBoxMediaSuffix.Enabled = checkBoxSuffix.Checked;
-            textBoxSubSuffix.Enabled = checkBoxSuffix.Checked;
-
-            FilterMediaFiles();
         }
     }
 }
